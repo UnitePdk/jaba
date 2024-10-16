d967934eb832a862b34c17cbc6cf67c4637e6257
UnitePdk <qkrrjs1515@gmail.com>
2024-10-16 19:16:54 +0900

    day01

---
+++ source/day01/src/exam01/ex01.java
@@ -0,0 +1,12 @@
+package exam01;
+
+public class ex01 {
+    public static void main(String[] args){
+        int num; //변수 선언
+        num = 10; //변수에 값 대입
+
+        int num2=20; //변수 선언과 동시에 초기화
+
+        System.out.println(num2);
+    }
+}
---
+++ source/day01/src/exam01/ex02.java
@@ -0,0 +1,14 @@
+package exam01;
+
+public class ex02 {
+    /**
+     *
+     * @param args
+     * 설명 주석
+     */
+    public static void main(String[] args) { //psvm 치면 바로나옴 엌ㅋ
+        int numOfStudent;
+
+    }
+
+}
